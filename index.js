36d900e7dc15910afce432ecf29cf4529d0c1ee4
Nasrullah <nasrullah01n@gmail.com>
2022-08-25 19:25:52 +0800

    Changed number of recent projects to 6 and changed frequency of updates to once an hour

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const fs = require("fs");
         return 1;
       })
       .filter(item => item.name !== "Coeeter")
-      .slice(0, 8)
+      .slice(0, 6)
       .map(item => {
         return `<a href="https://github.com/Coeeter/${item.name}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=Coeeter&theme=github_dark&hide_border=true&repo=${item.name}"/></a>`;
       })
