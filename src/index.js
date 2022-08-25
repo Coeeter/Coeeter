3c9a8a30f927f530a4e1d4b621100e84290d4e59
Nasrullah <nasrullah01n@gmail.com>
2022-08-25 23:55:41 +0800

    Added Github Token Authorization

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 const axios = require("axios");
 const fs = require("fs");
+require("dotenv").config();
 
-try {
-  const reposResponse = JSON.parse(fs.readFileSync("output.json"));
-  const recentProjects = reposResponse
-    .sort((a, b) => {
-      if (a["created_at"] > b["created_at"]) return -1;
-      return 1;
-    })
-    .filter(item => item.name !== "Coeeter")
-    .slice(0, 6)
-    .map(item => {
-      return `<a href="https://github.com/Coeeter/${item.name}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=Coeeter&theme=github_dark&hide_border=true&repo=${item.name}"/></a>`;
-    })
-    .join("");
-  fs.readFile("src/assets/template.txt", (err, data) => {
-    if (err) return console.log(err);
-    const text = data.toString() + "\n" + recentProjects;
-    fs.writeFile("README.md", text, err => {
-      if (err) console.log(err);
+(async () => {
+  try {
+    const githubToken = process.env.GITHUB_TOKEN;
+    const reposResponse = await axios.get(
+      "https://api.github.com/users/Coeeter/repos",
+      githubToken
+        ? {
+            headers: {
+              authorization: "Bearer " + githubToken,
+            },
+          }
+        : null
+    );
+    const recentProjects = reposResponse.data
+      .sort((a, b) => {
+        if (a["created_at"] > b["created_at"]) return -1;
+        return 1;
+      })
+      .filter(item => item.name !== "Coeeter")
+      .slice(0, 6)
+      .map(item => {
+        return `<a href="https://github.com/Coeeter/${item.name}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=Coeeter&theme=github_dark&hide_border=true&repo=${item.name}"/></a>`;
+      })
+      .join("");
+    fs.readFile("src/assets/template.txt", (err, data) => {
+      if (err) return console.log(err);
+      const text = data.toString() + "\n" + recentProjects;
+      fs.writeFile("README.md", text, err => {
+        if (err) console.log(err);
+      });
     });
-  });
-} catch (e) {
-  console.log(e);
-}
+  } catch (e) {
+    console.log(e);
+  }
+})();
