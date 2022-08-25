3f32ac2bf34ebecda3ca3d33dbad8206619eb2fe
Nasrullah <nasrullah01n@gmail.com>
2022-08-25 23:44:43 +0800

    Moved api request to workflow actions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 const axios = require("axios");
 const fs = require("fs");
 
-(async () => {
-  try {
-    const reposResponse = await axios.get(
-      "https://api.github.com/users/Coeeter/repos"
-    );
-    const recentProjects = reposResponse.data
-      .sort((a, b) => {
-        if (a["created_at"] > b["created_at"]) return -1;
-        return 1;
-      })
-      .filter(item => item.name !== "Coeeter")
-      .slice(0, 6)
-      .map(item => {
-        return `<a href="https://github.com/Coeeter/${item.name}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=Coeeter&theme=github_dark&hide_border=true&repo=${item.name}"/></a>`;
-      })
-      .join("");
-    fs.readFile("src/assets/template.txt", (err, data) => {
-      if (err) return console.log(err);
-      const text = data.toString() + "\n" + recentProjects;
-      fs.writeFile("README.md", text, err => {
-        if (err) console.log(err);
-      });
+try {
+  const reposResponse = JSON.parse(fs.readFileSync("output.json"));
+  const recentProjects = reposResponse
+    .sort((a, b) => {
+      if (a["created_at"] > b["created_at"]) return -1;
+      return 1;
+    })
+    .filter(item => item.name !== "Coeeter")
+    .slice(0, 6)
+    .map(item => {
+      return `<a href="https://github.com/Coeeter/${item.name}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=Coeeter&theme=github_dark&hide_border=true&repo=${item.name}"/></a>`;
+    })
+    .join("");
+  fs.readFile("src/assets/template.txt", (err, data) => {
+    if (err) return console.log(err);
+    const text = data.toString() + "\n" + recentProjects;
+    fs.writeFile("README.md", text, err => {
+      if (err) console.log(err);
     });
-  } catch (e) {
-    console.log(e);
-  }
-})();
+  });
+} catch (e) {
+  console.log(e);
+}
